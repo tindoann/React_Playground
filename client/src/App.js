19e0ba75d4d59169d704b4b60784d9ba50381fbe
Tin <tin.doann@gmail.com>
2021-07-08 14:43:39 -0400

    Build: toggle component

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Toggle from "./components/toggle";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Toggle />
+    </div>
+  );
+}
+
+export default App;
